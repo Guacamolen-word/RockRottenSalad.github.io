6a96a998e66ec5a9eafb917826e3cd4b5a23427e
RockRottenSalad <97804775+RockRottenSalad@users.noreply.github.com>
2022-02-08 08:21:33 +0000

    Update addfunction.js

--- addfunction.js
+++ addfunction.js
@@ -1,6 +1,6 @@
    function add() {
-      var f = parseString(document.getElementsByName("input1")[0].value);
-      var s =parseString(document.getElementsByName("input2")[0].value);
+      var f = parseInt(document.getElementsByName("input1")[0].value);
+      var s =parseInt(document.getElementsByName("input2")[0].value);
       var result = f+s;
       document.getElementsByName("display")[0].value= result;
    }
